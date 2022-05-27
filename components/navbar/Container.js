f5f9443d6d139239303c0fabf4f6d8847ab7dc44
ali yaghuti <a.yaghuti1976@gmail.com>
2022-05-27 16:28:27 +0430

    applying changes

--- components/navbar/Container.js
+++ components/navbar/Container.js
@@ -16,7 +16,7 @@ import DarkModeSwitch from "./DarkModeSwitch";
 import Footer from "../Footer";
 
 const Container = ({ children }) => {
-  const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode(light);
   const [display, changeDisplay] = useState("none");
 
   const bgColor = {
