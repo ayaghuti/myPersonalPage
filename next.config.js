1aa137470e41ba2f63a630aa451c27a0b701f30c
Ali Yaghuti <a.yaghuti1976@gmail.com>
2023-06-03 15:07:23 +0430

    Merge branch 'main' of https://github.com/ayaghuti/myPersonalPage

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
+
 const { i18n } = require("./next-i18next.config");
-=======
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // ignoreDuringBuilds: true,
 };
->>>>>>> 78d52242a3b21e21cc478a940e5c3d04fa314b5f
 
 module.exports = {
   i18n,
